5165f3300ccaf5d63a45d9fcce2a87e4a3877136
Sara-Anni <sanni11213@gmail.com>
2025-11-10 22:49:26 +0600

    added ticket cards and task section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+import { Suspense } from 'react';
 import './app.css'
 import Banner from './components/banner';
 import Body from './components/body';
 import Navbar from './components/navbar';
 
- function App() {
-  
 
+const fetchTickets = async() =>{
+  const res = await fetch("/tickets.json")
+  return res.json()
+}
+
+ function App() {
+  const ticketsPromise = fetchTickets()
   return (
     <>
     <div className="bg-white min-h-screen" data-theme="light">
       <Navbar></Navbar>
       <Banner></Banner>
-      <Body></Body>
+      <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+        <Body ticketsPromise={ticketsPromise}></Body>
+      </Suspense>
     </div>
      
     </>
--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,9 +1,85 @@
-import React from 'react';
 
-const Body = () => {
+
+import React, { use } from 'react';
+import './body.css'; 
+
+
+const Body = ({ ticketsPromise }) => {
+    const ticketsData = use(ticketsPromise);
+    console.log(ticketsData);
+    
+    const getStatusClass = (status) => {
+        switch (status.toLowerCase().replace('-', ' ')) {
+            case 'open':
+                return 'status-open';
+            case 'in-progress':
+                return 'status-in-progress';
+            case 'resolved': 
+            default:
+                return 'status-open'; 
+        }
+    };
+    
+    const getPriorityClass = (priority) => {
+        switch (priority.toLowerCase().replace('-', ' ')) {
+            case 'high priority':
+                return 'priority-high';
+            case 'medium priority':
+                return 'priority-medium';
+            case 'low priority':
+                return 'priority-low';
+            default:
+                return '';
+        }
+    };
+
     return (
-        <div>
+        <div className="main-content-wrapper"> 
+            <div className="tickets-grid-container">
+                {
+                    ticketsData.map(ticket => (
+                        <div className="ticket-card" key={ticket.id}>
+                            <div className="card-header">
+                                <h3 className="card-title">{ticket.title}</h3>
+                                <div className={`card-status ${getStatusClass(ticket.status)}`}>
+                                    {ticket.status}
+                                </div>
+                            </div>
+
+                            <p className="card-description">
+                                {ticket.description}
+                            </p>
+
+                            <div className="card-footer">
+                                <div className="details-left">
+                                    <span className="ticket-id">{ticket.id}</span>
+                                    <span className={`card-priority ${getPriorityClass(ticket.priority)}`}>
+                                        {ticket.priority}
+                                    </span>
+                                </div>
+                                <div className="details-right">
+                                    <span className="assignee">{ticket.customer}</span>
+                                    
+                                    <span className="date">📅 {ticket.createdAt}</span> 
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
             
+            <div className="task-sidebar">
+                <div className="task-status-card">
+                    <h3 className="sidebar-title">Task Status</h3>
+                    <p className="sidebar-text">Select a ticket to add to Task Status</p>
+                </div>
+
+                <div className="resolved-task-card">
+                    <h3 className="sidebar-title">Resolved Task</h3>
+                    <p className="sidebar-text">No resolved tasks yet.</p>
+                </div>
+            </div>
         </div>
     );
 };
